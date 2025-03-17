a4412c843e34ade1ac99c7182c82c20ec4b3c100
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-03-17 10:59:39 +0530

    [Loveleen] ADD: Adding a online food delivery system

---
+++ FoodDeliverySystem.java
@@ -0,0 +1,94 @@
+// Abstract class FoodItem
+abstract class FoodItem {
+    protected String itemName;
+    protected double price;
+    protected int quantity;
+
+    public FoodItem(String itemName, double price, int quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public abstract double calculateTotalPrice();
+
+    public void getItemDetails() {
+        System.out.println("Item: " + itemName + ", Price: " + price + ", Quantity: " + quantity);
+    }
+}
+
+// Interface for discountable items
+interface Discountable {
+    void applyDiscount(double discountPercentage);
+    double getDiscountDetails();
+}
+
+// VegItem subclass
+class VegItem extends FoodItem implements Discountable {
+    private double discount;
+
+    public VegItem(String itemName, double price, int quantity) {
+        super(itemName, price, quantity);
+    }
+
+    @Override
+    public double calculateTotalPrice() {
+        return (price * quantity) - discount;
+    }
+
+    @Override
+    public void applyDiscount(double discountPercentage) {
+        discount = (price * quantity) * (discountPercentage / 100);
+    }
+
+    @Override
+    public double getDiscountDetails() {
+        return discount;
+    }
+}
+
+// NonVegItem subclass
+class NonVegItem extends FoodItem implements Discountable {
+    private double extraCharge;
+    private double discount;
+
+    public NonVegItem(String itemName, double price, int quantity, double extraCharge) {
+        super(itemName, price, quantity);
+        this.extraCharge = extraCharge;
+    }
+
+    @Override
+    public double calculateTotalPrice() {
+        return (price * quantity + extraCharge) - discount;
+    }
+
+    @Override
+    public void applyDiscount(double discountPercentage) {
+        discount = (price * quantity + extraCharge) * (discountPercentage / 100);
+    }
+
+    @Override
+    public double getDiscountDetails() {
+        return discount;
+    }
+}
+
+// Main class to demonstrate polymorphism
+public class FoodDeliverySystem {
+    public static void main(String[] args) {
+        FoodItem[] order = {
+                new VegItem("Paneer Tikka", 200, 2),
+                new NonVegItem("Chicken Biryani", 350, 1, 50)
+        };
+
+        for (FoodItem item : order) {
+            item.getItemDetails();
+            if (item instanceof Discountable) {
+                Discountable discountableItem = (Discountable) item;
+                discountableItem.applyDiscount(10);
+                System.out.println("Discount Applied: " + discountableItem.getDiscountDetails());
+            }
+            System.out.println("Total Price: " + item.calculateTotalPrice() + "\n");
+        }
+    }
+}
